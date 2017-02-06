90cb5715915f5aaeeffd10c48bf903a58d427c00
Enrique Martín <emartinm@fdi.ucm.es>
2017-02-06 15:41:25 +0100

    Solución temporal para compilar C++ en Windows con Visual Studio. El código en explicaciones y preguntas aparece recuadrado.

--- src/main/java/es/ucm/innova/docentia/TutorialesInteractivos/model/language/CppLanguage.java
+++ src/main/java/es/ucm/innova/docentia/TutorialesInteractivos/model/language/CppLanguage.java
@@ -3,9 +3,12 @@ package es.ucm.innova.docentia.TutorialesInteractivos.model.language;
 import es.ucm.innova.docentia.TutorialesInteractivos.controller.Controller;
 import es.ucm.innova.docentia.TutorialesInteractivos.model.ConfigurationData;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by kike on 4/02/17.
@@ -34,11 +37,28 @@ public class CppLanguage extends Language {
     }
 
     protected ProcessBuilder getCompilationProcess(String sourcePath, String outputFilePath) {
-        ProcessBuilder pb;
+        ProcessBuilder pb = null;
         File f = new File(outputFilePath);
-        if ( this.compiler.contains("cl.exe") ) {
-            // Visual Studio
-            pb = new ProcessBuilder(this.compiler, sourcePath, "/Fe"+outputFilePath, "/Fo"+f.getParent());
+        File bat;
+        if ( this.compiler.toLowerCase().contains("vcvars32") ) {
+            // "Chapuza" para que funcione Visual Studio
+            // El problema es que no consigue compilar porque le falta el entorno adecuado para INCLUDE, LIB, etc.
+            // La posible solución es crear un script BAT que invoca primero al compiler (que es un script vcvars32.bat
+            // que carga el entorno) y luego invocar directamente a cl.exe con las opciones adecuadas.
+            // Es bastante chapuza pero no se me ocurre otra cosa...
+            try{
+                bat = File.createTempFile("compilation",".bat");
+                BufferedWriter bw = new BufferedWriter( new FileWriter(bat, true));
+                bw.write("CALL \"" + this.compiler + "\"");
+                bw.newLine();
+                bw.write("CALL cl.exe \"" + sourcePath + "\" /Fe" + outputFilePath + " /Fo" + f.getParent());
+                bw.newLine();
+                bw.close();
+                pb = new ProcessBuilder("cmd.exe", "/C\"" + bat.getAbsolutePath() + "\"" );
+                //this.compiler, sourcePath, "/Fe"+outputFilePath, "/Fo"+f.getParent());
+            } catch (Exception e ) {
+                Controller.log.warning("Imposible construir el BAT para compilar:" + e.getLocalizedMessage() );
+            }
         } else {
             // g++
             pb = new ProcessBuilder(this.compiler, sourcePath, "-std=c++11", "-o", outputFilePath);
