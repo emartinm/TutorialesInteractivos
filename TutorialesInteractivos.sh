82943ea16c47521eba048362569b5317f5ce608b
Enrique Martín <emartinm@fdi.ucm.es>
2017-10-12 11:02:06 +0200

    Actualizado lanazador para Linux

--- TutorialesInteractivos.sh
+++ TutorialesInteractivos.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Lanza la herramienta "Tutoriales Interactivos" 
+# Primero prueba la version de Java en el directorio ./jre
 
+export PATH=./jre64/bin:${PATH}
 JAVA_COMMAND=$(which java)
+echo `${JAVA_COMMAND} -version`
 
 ${JAVA_COMMAND} -jar target/TutorialesInteractivos-jar-with-dependencies.jar
