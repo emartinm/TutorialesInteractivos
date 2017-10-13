eda3202e3879338eb1236485f546e91599caecd0
Enrique Martín <emartinm@fdi.ucm.es>
2017-10-13 11:12:08 +0200

    Corregido directorio de JRE en Linux

--- TutorialesInteractivos.sh
+++ TutorialesInteractivos.sh
@@ -3,7 +3,7 @@
 # Lanza la herramienta "Tutoriales Interactivos" 
 # Primero prueba la version de Java en el directorio ./jre
 
-export PATH=./jre64/bin:${PATH}
+export PATH=./jre64_linux/bin:${PATH}
 JAVA_COMMAND=$(which java)
 echo `${JAVA_COMMAND} -version`
 
