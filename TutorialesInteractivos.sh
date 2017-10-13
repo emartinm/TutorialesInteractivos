ec8d02244b1312fc5432dfd948075f20c1d1e939
Enrique Martín <emartinm@fdi.ucm.es>
2017-10-13 12:33:23 +0200

    Actalizado lanzador de linux

--- TutorialesInteractivos.sh
+++ TutorialesInteractivos.sh
@@ -3,21 +3,21 @@
 # Lanza la herramienta "Tutoriales Interactivos" 
 # Primero prueba la version de Java en el directorio ./jre64_linux o ./jre64_mac
 
-SCRIPT_PATH=`realpath ${0}`
-TUTORIALES_PATH=`dirname ${SCRIPT_PATH}`
-echo ${TUTORIALES_PATH}
+#SCRIPT_PATH=`realpath ${0}`
+#TUTORIALES_PATH=`dirname ${SCRIPT_PATH}`
+#echo ${TUTORIALES_PATH}
+TUTORIALES_PATH=${PWD}
 OS=$(uname)
 if [ ${OS} == "Linux" ]
 then
     # Es un sistema Linux  
-    export PATH=${TUTORIALES_PATH}/jre/jre64_linux/bin:${PATH}    
+    export PATH="${TUTORIALES_PATH}/jre/jre64_linux/bin":${PATH}
 else
     # Es un sistema Darwin (Mac)
-    export PATH=${TUTORIALES_PATH}/jre/jre64_mac/Contents/Home/bin:${PATH}
+    export PATH="${TUTORIALES_PATH}/jre/jre64_mac/Contents/Home/bin":${PATH}
 fi
 
-echo ${PATH}
 JAVA_COMMAND=$(which java)
 echo ${JAVA_COMMAND}
 
-${JAVA_COMMAND} -jar ${TUTORIALES_PATH}/target/TutorialesInteractivos-jar-with-dependencies.jar
+"${JAVA_COMMAND}" -jar "${TUTORIALES_PATH}/target/TutorialesInteractivos-jar-with-dependencies.jar"
